7f974f95c34bcb5c112b2c19864a46e8fafe3072
Joao Lucas Camargo <j.lucascamargo@gmail.com>
2023-09-29 15:57:44 -0300

    feat: add tailwind config

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -18,12 +18,21 @@ const handleSubmit = async () => {
 
 
   return (
-    <div>
-        <h1>Join</h1>
+    <div className=' flex, flex-col, align-items-center, justify-content-center'>
+        <h1 className='font-bold text-4xl mb-5, text-center, text-gray-800'>
 
-        <input type="text" ref={usernameRef} placeholder='Enter your name' />
 
-        <button onClick={() => handleSubmit()}>Enter</button>
+
+          Entrar
+
+
+          </h1>
+
+        <input className='border border-gray-400 p-2, w-full, mb-5, rounded-md , text-gray-800, bg-gray-100, outline-none, focus:outline-none, focus:shadow-outline'
+         type="text" ref={usernameRef} placeholder='Enter your name' />
+
+        <button className='flex, flex-col, align-items-center, justify-content-center, border border-gray-400 p-2, w-full, mb-5, rounded-md , text-gray-800, bg-gray-100, outline-none, focus:outline-none, focus:shadow-outline'
+         onClick={() => handleSubmit()}>Enter</button>
     </div>
   )
 }
